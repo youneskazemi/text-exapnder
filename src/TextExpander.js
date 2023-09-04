496632d20f8801377116db4355b02885e7035f8a
Younes Kazemi <youneskazemi9798@gmail.com>
2023-09-04 20:20:47 +0330

    add proptypes

--- src/TextExpander.js
+++ src/TextExpander.js
@@ -1,13 +1,23 @@
 import { useState } from "react";
+import { PropTypes } from "prop-types";
 
 const textStyle = {
   paddingRight: "5px",
 };
 
+TextExpander.propTypes = {
+  expandButtonText: PropTypes.string,
+  className: PropTypes.string,
+  buttonColor: PropTypes.string,
+  collapsedNumWords: PropTypes.number,
+  collapseButtonText: PropTypes.string,
+  expanded: PropTypes.bool,
+};
+
 function TextExpander({
   children,
   expandButtonText = "Show more",
-  className = {},
+  className = "",
   buttonColor,
   collapsedNumWords = 65,
   collapseButtonText = "Show less",
